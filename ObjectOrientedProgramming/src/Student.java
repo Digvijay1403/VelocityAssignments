26a3693f77e002f703a83107e2c8ea67f117780f
Digvijay k32012 <digvijaypatil1403@gmail.com>
2023-02-07 14:03:58 -0500

    Assignment #3

---
+++ ObjectOrientedProgramming/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    void getStudentName() {
+        System.out.println("Name of student is " + name);
+    }
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.name = "Digvijay";
+        student.getStudentName();
+    }
+}
