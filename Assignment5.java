861fc0586a90a80ae75c97b39a37be61124798cc
Digvijay k32012 <digvijaypatil1403@gmail.com>
2023-02-10 07:39:51 -0500

    Assignment 4

---
+++ Assignment5.java
@@ -0,0 +1,30 @@
+public class Assignment5 {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    Assignments-5
+//            1.write a program to check the number is positive or negative.
+//2.write a program  to check whether  year is leap year or not. (If else stmt).
+//            3.write a program to find out maximum number among three number(if else if ladder statement)
+//4.Write a program to print month of year. Case 1 January case 2 February case n....use switch case
+//            5. Write a program to check whether number is even or odd.
+//6. Write the program to check whether the no is greater than 100.
+//            7.  Write the program to print the square of any number.
+//            8. Write a program to swap the two numbers.
+//    Note- Once you are done with your assignments keep it with you don't send me. I will share one PDF to you which will have answer of all assignments. All you need to do is self review check your answers and verify with sheet... Correct them if wrong.
+//    VIMP - Do not use Google it will reduce your chances of getting placed.
