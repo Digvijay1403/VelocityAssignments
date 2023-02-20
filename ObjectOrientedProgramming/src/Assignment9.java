47f9fb26e5a5ce692c65757c3b194193dd3cce92
Digvijay k32012 <digvijaypatil1403@gmail.com>
2023-02-20 17:17:41 -0500

    Assignment 9

---
+++ ObjectOrientedProgramming/src/Assignment9.java
@@ -0,0 +1,28 @@
+package ObjectOrientedProgramming.src;
+
+import Practice.A;
+
+public class Assignment9 {
+    static int count;
+    Assignment9(){
+        count++;
+        System.out.println("Object Created"+ count);
+    }
+    public static void main(String[] args) {
+        Assignment9 assignment9_1 = new Assignment9();
+        Assignment9 assignment9_2 =  new Assignment9();
+        Assignment9 assignment9_3 = new Assignment9();
+        Assignment9 assignment9_4 =  new Assignment9();
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
